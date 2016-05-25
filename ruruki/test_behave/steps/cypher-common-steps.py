e5910899718b153144cf732a155d9a46b2593764
Jenda Mudron <jenmud@gmail.com>
2016-05-25 21:55:27 +1000

    added in the missing doco on all the step functions

--- ruruki/test_behave/steps/cypher-common-steps.py
+++ ruruki/test_behave/steps/cypher-common-steps.py
@@ -6,11 +6,27 @@ from ruruki.parsers import cypher_parser
 
 @when("we parse the string {query} through the parse function")
 def parse_the_query_string(context, query):
+    """
+    Parse the given query string.
+
+    :param context: Context object share between all the setups.
+    :type context: :class:`behave.runner.Context`
+    :param query: Query string to parse.
+    :type query: :class:`str`
+    """
     context.result = cypher_parser.parse(query, expr=context.expr)
 
 
 @then("it should transform the parsing result into {result}")
 def validate_result(context, result):
+    """
+    Validate the parsed result.
+
+    :param context: Context object share between all the setups.
+    :type context: :class:`behave.runner.Context`
+    :param result: Query parsed result.
+    :type result: :class:`dict`
+    """
     result = eval(result)
     assert_that(
         context.result
