dfd3e3c577224e870eb09ec5069cd011ead951b0
Jenda Mudron <jenmud@gmail.com>
2016-05-25 21:37:38 +1000

    moved all the parser features and steps directly into the test_behave because of parsing issue using multiple paths

--- ruruki/test_behave/parsers/steps/cypher-vertex-grammar-expressions.py
+++
@@ -1,7 +0,0 @@
-from behave import given
-from ruruki.parsers import cypher_parser
-
-
-@given("we have a vertex grammar expression")
-def setup_vertex_expression(context):
-    context.expr = cypher_parser.vertex
